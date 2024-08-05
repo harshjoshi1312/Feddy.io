f822d291af8bb50e5fabd9cb0adbd8cb85b866dd
harshjoshi1312 <joshiharsh1312@gmail.com>
2024-08-05 20:52:48 +0530

    :tea: done messaging part

--- src/app/api/send-messages/route.ts
+++ src/app/api/send-messages/route.ts
@@ -20,12 +20,39 @@ export async function POST(request: Request) {
       );
     }
 
-
     // is User accepting the Messages
     if (!user.isAcceptingMessage) {
-        
+      return Response.json(
+        {
+          success: false,
+          message: "User is not accepting messages",
+        },
+        {
+          status: 404,
+        }
+      );
     }
-  } catch (error) {}
-}
 
-// 35.20 next js aggregation pipeline applied succesfully
+    const newMessage = { content, createdAt: new Date() };
+    user.message.push(newMessage as Message);
+    await user.save();
+    return Response.json(
+      {
+        success: true,
+        message: "message sent succesfully",
+      },
+      {
+        status: 404,
+      }
+    );
+  } catch (error) {
+    console.log("error adding messages");
+    return Response.json(
+      {
+        success: false,
+        message: "internal server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
